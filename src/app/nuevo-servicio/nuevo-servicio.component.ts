30f14eb92d2c24b2d6e9893fc117fc901b9bbf6d
Andres Bolivar <andresfebo187@gmail.com>
2019-12-11 20:28:55 -0500

    change with var

--- src/app/nuevo-servicio/nuevo-servicio.component.ts
+++ src/app/nuevo-servicio/nuevo-servicio.component.ts
@@ -14,6 +14,7 @@ export class NuevoServicioComponent implements OnInit {
   formGroupServicio;
   servicio: Servicio;
   serviciosData: Servicio[];
+  servicioId: 0;
 
   constructor(private formBuilder: FormBuilder,private router: Router, private service: ApiConsumerService) { }
 
